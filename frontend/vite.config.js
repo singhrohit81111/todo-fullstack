58141f91ee3caba81181553148b99c7ec4027fb9
Rohit Singh <singhrohit81111@gmail.com>
2024-01-05 01:02:23 +0530

    removed proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    proxy:{
-      '/api': {
-        target: 'https://todo-backend-u5ci.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure:true
-      }
-    }
-  }
+  plugins: [react()]
 })
